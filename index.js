29c1a541767bdb46179dc5e8cce6a94eabfe8569
Your Name~ <you@example.com>
2023-09-30 20:54:26 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-var button=document.querySelector(button);
+var buttn= document.querySelectorAll("button");
+console.log(buttn);
+var inpt=document.querySelector("input");
+var str=" ";
+for(var i=0;i<buttn.length;i++)
+{
+    buttn[i].addEventListener("click", function(){
+         if(this.innerHTML=="C")
+         {
+            str=" ";
+            inpt.value=str;
+         }
+         else if(this.innerHTML=="=")
+         {
+            inpt.value=eval(str);
+         }
+         else{
+            str=str+this.innerHTML;
+            inpt.value=str;
+         }
+    });
+}
+document.addEventListener("keydown", function(event){
+
+        if(event.key=="C" || event.key=="c")
+        {
+           str=" ";
+           inpt.value=str;
+        }
+        else if(event.key=="="|| event.key=="Enter")
+        {
+           inpt.value=eval(str);
+        }
+        else{
+           str=str+event.key;
+           inpt.value=str;
+        }
+     
+    
+});
+
+    
